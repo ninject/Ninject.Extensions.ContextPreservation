55199f926f0dcd11007bd411de41dff404902f2f
Remo Gloor <remo.gloor@bbv.ch>
2011-12-11 03:31:16 +0100

    Updated to the latest build scripts and added partial trust for .NET 3.5

--- src/Ninject.Extensions.ContextPreservation/Properties/AssemblyInfo.cs
+++ src/Ninject.Extensions.ContextPreservation/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -27,3 +28,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("63ada681-c514-40c2-8f36-65de78db1219")]
+
+#if !NO_PARTIAL_TRUST
+[assembly: AllowPartiallyTrustedCallers]
+#endif
