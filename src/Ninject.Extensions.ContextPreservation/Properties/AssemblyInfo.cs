fa60b094ebfdea8b6c00c51e9ba562724df37253
Remo Gloor <remo.gloor@bbv.ch>
2011-12-27 20:26:32 +0100

    Added assembly description for nuget

--- src/Ninject.Extensions.ContextPreservation/Properties/AssemblyInfo.cs
+++ src/Ninject.Extensions.ContextPreservation/Properties/AssemblyInfo.cs
@@ -25,6 +25,7 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ninject.Extensions.ContextPreservation")]
+[assembly: AssemblyDescriptionAttribute("Ninject extension that preserves the context over factories (the parent of the new request is the factory request)")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("63ada681-c514-40c2-8f36-65de78db1219")]
